fc19630055017f01a46672e16cedf99bfb4ede5b
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-01-18 21:06:00 +0700

    Add files via upload

--- scripts/register.js
+++ scripts/register.js
@@ -11,68 +11,63 @@ class User {
 const KEY = "USER_ARRAY";
 const userArr = JSON.parse(getFromStorage(KEY)) || [];
 
-const inputFirstName = document.getElementById("input-firstname");
-const inputLastName = document.getElementById("input-lastname");
-const inputUserName = document.getElementById("input-username");
-const inputPassword = document.getElementById("input-password");
-const inputPasswordConfirm = document.getElementById("input-password-confirm");
 const submitBtn = document.getElementById("btn-submit");
 submitBtn.addEventListener("click", () => {
-  const userData = {
-    firstName: inputFirstNames.value,
-    lastName: inputLastNames.value,
-    username: inputUsername.value,
-    password: inputPassword.value,
-    confirm: inputPasswordConfirm.value,
-  };
+  const inputFirstName = document.getElementById("input-firstname").value;
+  const inputLastName = document.getElementById("input-lastname").value;
+  const inputUserName = document.getElementById("input-username").value;
+  const inputPassword = document.getElementById("input-password").value;
+  const inputPasswordConfirm = document.getElementById(
+    "input-password-confirm"
+  ).value;
+
   let validate = true;
   if (
-    !data.firstName ||
-    !data.lastName ||
-    !data.username ||
-    !data.password ||
-    !data.confirm
+    !inputFirstName ||
+    !inputLastName ||
+    !inputUserName ||
+    !inputPassword ||
+    !inputPasswordConfirm
   ) {
     alert("Please fill all fields");
     validate = false;
   } else {
     for (let i = 0; i < useArr.length; i++) {
-      if (data.username == useArr[i].username) {
+      if (inputUserName == useArr[i].username) {
         alert("Username must unique!");
         validate = false;
       }
     }
-    if (!data.password.match(/[a-zA-Z0-9]{8,}/)) {
+    if (!inputPassword.length < 8) {
       alert("Password must be at least 8 characters long");
       validate = false;
     }
-    if (data.password != data.confirm) {
+    if (inputPassword != inputPasswordConfirm) {
       alert("Password and confirm password do not match");
       validate = false;
     }
   }
   if (validate) {
     let user = new User(
-      data.firstName,
-      data.lastName,
-      data.email,
-      data.password,
-      data.password,
-      data.confirm
+      inputFirstName,
+      inputLastName,
+      inputUserName,
+      inputPassword,
+      inputPasswordConfirm
     );
-    const data = parseUsers(user);
-    userArr.push(data);
+    // const data = parseUsers(user);
+    userArr.push(user);
     saveToStorage(KEY, JSON.stringify(userArr));
     window.location.href = "../pages/login.html";
   }
 });
-function parseUser(userData) {
-  const user = new User(
-    userData.firstname,
-    userData.lastname,
-    userData.username,
-    userData.password
-  );
+// function parseUser(userData) {
+//   const user = new User(
+//     userData.firstname,
+//     userData.lastname,
+//     userData.username,
+//     userData.password
+//   );
 
-  return user;
-}
+//   return user;
+// }
