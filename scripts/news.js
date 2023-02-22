3189c4c52c43fbad02069b6a4d5aa97b268b6ef6
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-02-22 14:21:24 +0700

    Add files via upload

--- scripts/news.js
+++ scripts/news.js
@@ -11,6 +11,27 @@ const numberPage = document.querySelector("#page-num");
 
 ////////////////////////////////
 
+const news = async function (country, category, pageSize, page) {
+  try {
+    let totalPages = 0;
+    let arts = [];
+    const res = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&pageSize=${pageSize}&page=${page}&apiKey=4aeb63983e194023a4e694205173d98d`
+    );
+
+    const data = await res.json();
+    console.log(data);
+    arts = await data.articles;
+    totalPages = data.totalResults;
+    console.log(arts);
+    console.log(totalPages);
+    // updatePage(numberNewsPerPage);
+  } catch (err) {
+    console.error(err);
+  }
+};
+news("us", "business", 20, 4);
+
 const updatePage = function (numberNews) {
   news(apiKeys);
   console.log(arts);
@@ -42,112 +63,87 @@ const updatePage = function (numberNews) {
   }
   newsContainer.innerHTML = html;
 };
-////
+// ////
 
-function prevnextDisplay(prevBtn, nextBtn, maxPage) {
-  //Điều chỉnh nút previous và nút next
-  if (curPage == 1) {
-    //Nếu trang hiện tại là 1
-    prevBtn.classList.add("toast"); //Ẩn nút previous đi
-    prevBtn.classList.add("disabled"); //Hủy luôn cả hiệu ứng khi rê chuột vào
-  }
-  if (curPage == maxPage) {
-    //Nếu trang hiện tại là lớn nhất
-    nextBtn.classList.add("toast"); //Ẩn nút next đi
-    nextBtn.classList.add("disabled"); //Hủy luôn cả hiệu ứng khi rê chuột vào
-  }
-  if (curPage > 1 && curPage < maxPage) {
-    //Nếu trang hiện tại nằm giữa 1 và lớn nhất  thì hiện cả 2 nút previous và next lên cũng như khôi hiệu ứng cho chúng
-    nextBtn.classList.remove("toast");
-    nextBtn.classList.remove("disabled");
-    prevBtn.classList.remove("disabled");
-    prevBtn.classList.remove("toast");
-  }
-}
-
-const news = async function (key) {
-  //country, category, pageSize, page,
-  try {
-    let totalPages = 0;
-    let arts = [];
-    let totalResults = 0;
-    const res = await fetch(
-      `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${key}`
-    );
-
-    const data = await res.json();
-    console.log(data);
-    arts = await data.articles;
-    totalPages = data.totalResults;
-    console.log(arts);
-    console.log(totalPages);
-    updatePage(numberNewsPerPage);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const con = "un";
-const cata = "";
-const size = 50;
-const pages = 5;
-const apiKeys = "4aeb63983e194023a4e694205173d98d";
-// gọi hàm news
-
-let currentPage = 1;
-let numberNewsPerPage = 5;
-
-//con, cata, size, pages,
-
-previousButton.addEventListener("click", function () {
-  if (currentPage > 1) {
-    currentPage--;
-    news(apiKeys);
-  }
-});
-
-nextButton.addEventListener("click", function () {
-  if (currentPage * numberNewsPerPage < 20) {
-    currentPage++;
-    news(apiKeys);
-  }
-});
-
-const paginate = (data) => {
-  const itemsPerPage = 5;
-  const numberOffPages = Math.ceil(numberNewsPerPage / itemsPerPage);
-  const paginatedData = Array.from({ length: numberOffPages }, (...index) => {
-    const start = index * itemsPerPage;
-    return data.slice(start, start + itemsPerPage);
-  });
-};
-// tối cần làm cái gì đây nhỉ:
-
-/* 
-   đầu tiên tôi cần tạo tham số newsInPage,để quy định số news sẽ hiển thị trên một trang,
-   - sau sẽ truyền giá trị đầu và giá trị đầu và giá trị cuối vào hàm render
-  
-  
-  
-  */
-// Update the value of the "page" parameter for the API call
-// ...
-
-// Fetch the data for the new page
-// ...
-
-// Update the UI with the new data
-// ...
-
-// Show or hide the "Previous" and "Next" buttons based on the current page and total results
-// if (currentPage === 1) {
-//   previousButton.style.display = "none";
-// } else {
-//   previousButton.style.display = "block";
+// function prevnextDisplay(prevBtn, nextBtn, maxPage) {
+//   //Điều chỉnh nút previous và nút next
+//   if (curPage == 1) {
+//     //Nếu trang hiện tại là 1
+//     prevBtn.classList.add("toast"); //Ẩn nút previous đi
+//     prevBtn.classList.add("disabled"); //Hủy luôn cả hiệu ứng khi rê chuột vào
+//   }
+//   if (curPage == maxPage) {
+//     //Nếu trang hiện tại là lớn nhất
+//     nextBtn.classList.add("toast"); //Ẩn nút next đi
+//     nextBtn.classList.add("disabled"); //Hủy luôn cả hiệu ứng khi rê chuột vào
+//   }
+//   if (curPage > 1 && curPage < maxPage) {
+//     //Nếu trang hiện tại nằm giữa 1 và lớn nhất  thì hiện cả 2 nút previous và next lên cũng như khôi hiệu ứng cho chúng
+//     nextBtn.classList.remove("toast");
+//     nextBtn.classList.remove("disabled");
+//     prevBtn.classList.remove("disabled");
+//     prevBtn.classList.remove("toast");
+//   }
 // }
 
-// if (currentPage * pageSize >= totalResults) {
-//   nextButton.style.display = "none";
-// } else {
-//   nextButton.style.display = "block";
-// }
+// const con = "un";
+// const cata = "";
+// const size = 50;
+// const pages = 5;
+// // gọi hàm news
+
+// let currentPage = 1;
+// let numberNewsPerPage = 5;
+
+// //con, cata, size, pages,
+
+// previousButton.addEventListener("click", function () {
+//   if (currentPage > 1) {
+//     currentPage--;
+//     news(apiKeys);
+//   }
+// });
+
+// nextButton.addEventListener("click", function () {
+//   if (currentPage * numberNewsPerPage < 20) {
+//     currentPage++;
+//     news(apiKeys);
+//   }
+// });
+
+// const paginate = (data) => {
+//   const itemsPerPage = 5;
+//   const numberOffPages = Math.ceil(numberNewsPerPage / itemsPerPage);
+//   const paginatedData = Array.from({ length: numberOffPages }, (...index) => {
+//     const start = index * itemsPerPage;
+//     return data.slice(start, start + itemsPerPage);
+//   });
+// };
+// // tối cần làm cái gì đây nhỉ:
+
+// /*
+//    đầu tiên tôi cần tạo tham số newsInPage,để quy định số news sẽ hiển thị trên một trang,
+//    - sau sẽ truyền giá trị đầu và giá trị đầu và giá trị cuối vào hàm render
+
+//   */
+// // Update the value of the "page" parameter for the API call
+// // ...
+
+// // Fetch the data for the new page
+// // ...
+
+// // Update the UI with the new data
+// // ...
+
+// // Show or hide the "Previous" and "Next" buttons based on the current page and total results
+// // if (currentPage === 1) {
+// //   previousButton.style.display = "none";
+// // } else {
+// //   previousButton.style.display = "block";
+// // }
+
+// // if (currentPage * pageSize >= totalResults) {
+// //   nextButton.style.display = "none";
+// // } else {
+// //   nextButton.style.display = "block";
+// // }
