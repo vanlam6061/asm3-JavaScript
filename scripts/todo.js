fe8150842574729db7a5ca181a5ba1bff33ed44c
vanlam6061 <119390647+vanlam6061@users.noreply.github.com>
2023-03-07 16:53:44 +0700

    Add files via upload

--- scripts/todo.js
+++ scripts/todo.js
@@ -4,6 +4,7 @@ const addBtn = document.getElementById("btn-add");
 const inputTask = document.getElementById("input-task");
 const deleteBtn = document.getElementById("btn-delete");
 const dodoItem = document.getElementById("toggle-dodo");
+const todoContainer = document.getElementById("todo-list");
 
 // create todo class
 
@@ -16,16 +17,17 @@ class Todo {
 }
 
 // Get a list from local storage
-const KEY_TODO = "TODO";
+const KEY_TODO = "TO_DO";
 const todoArr = JSON.parse(getFromStorage(KEY_TODO)) || [];
-const loginUser = JSON.parse(getFromStorage(KEY_LOGIN_USER)) || [];
+const keyLoginUser = "USER_LOGIN";
+const loginUser = JSON.parse(getFromStorage(keyLoginUser)) || [];
 
 // Trigger the add button and CREATE a new Todo Instance
 
 addBtn.addEventListener("click", () => {
   const todoData = {
     task: inputTask.value,
-    owner: data.username,
+    owner: loginUser.username,
     isDone: true,
   };
   let validate = true;
@@ -34,10 +36,29 @@ addBtn.addEventListener("click", () => {
     validate = false;
   }
   if (validate) {
-    let addTodo = new Todo(todoData.task, todoData.isDone);
-    const data = parseUsers(user);
-    userArr.push(data);
-    saveToStorage(KEY, JSON.stringify(userArr));
-    window.location.href = "../pages/login.html";
+    let newTodo = new Todo(todoData.task, todoData.owner, todoData.isDone);
+    todoArr.push(newTodo);
+    saveToStorage(KEY_TODO, JSON.stringify(todoArr));
+    renderData();
   }
 });
+
+const renderData = function () {
+  let html = "";
+  todoArr.map((todo) => {
+    html += `
+    <li>${todo.task}<span class="close" id ="btn-delete" >×</span></li>`;
+  });
+  todoContainer.innerHTML = html;
+};
+
+// function parseUser(todoData) {
+//   const user = new Todo(
+//     todoData.firstName,
+//     todoData.lastName,
+//     todoData.userName,
+//     todoData.password
+//   );
+
+//   return user;
+// }
